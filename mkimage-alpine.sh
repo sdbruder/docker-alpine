f593b06d2943ea840aa36cf2d40f868955c2f680
Eivind Uggedal <eivind@uggedal.com>
2014-04-13 13:30:00 +0000

    apkv

--- mkimage-alpine.sh
+++ mkimage-alpine.sh
@@ -7,3 +7,8 @@ REPO=$MIRROR/$REL/main/$ARCH
 
 TMP=$(mktemp -d /tmp/alpine-docker-XXXXXXXXXX)
 trap "rm -r $TMP" EXIT TERM INT
+
+apkv() {
+  curl -s $REPO/APKINDEX.tar.gz | tar -Oxz | grep '^P:apk-tools-static$' -A1 |
+    tail -n1 | cut -d: -f2
+}
