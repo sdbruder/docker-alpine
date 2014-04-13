4c32518a3d004e6e866ffdbac559c164a2f0fea7
Eivind Uggedal <eivind@uggedal.com>
2014-04-13 13:29:35 +0000

    tmp

--- mkimage-alpine.sh
+++ mkimage-alpine.sh
@@ -4,3 +4,6 @@ REL=${REL:-edge}
 ARCH=${ARCH:-x86_64}
 MIRROR=${MIRROR:-http://nl.alpinelinux.org/alpine}
 REPO=$MIRROR/$REL/main/$ARCH
+
+TMP=$(mktemp -d /tmp/alpine-docker-XXXXXXXXXX)
+trap "rm -r $TMP" EXIT TERM INT
