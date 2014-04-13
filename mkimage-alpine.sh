036abeb942e979f24895338fb7b0d7541866498e
Eivind Uggedal <eivind@uggedal.com>
2014-04-13 13:36:12 +0000

    make repo var usable in configs

--- mkimage-alpine.sh
+++ mkimage-alpine.sh
@@ -3,14 +3,14 @@
 REL=${REL:-edge}
 ARCH=${ARCH:-x86_64}
 MIRROR=${MIRROR:-http://nl.alpinelinux.org/alpine}
-REPO=$MIRROR/$REL/main/$ARCH
+REPO=$MIRROR/$REL/main
 
 TMP=$(mktemp -d /tmp/alpine-docker-XXXXXXXXXX)
 trap "rm -r $TMP" EXIT TERM INT
 
 apkv() {
-  curl -s $REPO/APKINDEX.tar.gz | tar -Oxz | grep '^P:apk-tools-static$' -A1 |
+  curl -s $REPO/$ARCH/APKINDEX.tar.gz | tar -Oxz | grep '^P:apk-tools-static$' -A1 |
     tail -n1 | cut -d: -f2
 }
 
-curl -s $REPO/apk-tools-static-$(apkv).apk | tar -xz -C $TMP sbin/apk.static
+curl -s $REPO/$ARCH/apk-tools-static-$(apkv).apk | tar -xz -C $TMP sbin/apk.static
